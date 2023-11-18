16dc1c43f529f25f1c88c7db5cf4e9c94d70d481
priyanka387 <priyankapathak222@gmail.com>
2023-11-18 23:15:35 +0530

    Updating github repo

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the model
+with open("model/model.pkl", "rb") as model_file:
+    model = pickle.load(model_file)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json(force=True)
+
+    # Preprocess the input data
+    age = data["age"]
+    sex = data["sex"]
+    bmi = data["bmi"]
+    children = data["children"]
+    smoker = data["smoker"]
+    region_northwest = data["region_northwest"]
+    region_southeast = data["region_southeast"]
+    region_southwest = data["region_southwest"]
+
+    # Make a prediction
+    input_features = np.array(
+        [
+            [
+                age,
+                sex,
+                bmi,
+                children,
+                smoker,
+                region_northwest,
+                region_southeast,
+                region_southwest,
+            ]
+        ]
+    )
+    prediction = model.predict(input_features)
+
+    # Format the prediction as JSON and return
+    output = {"prediction": prediction.tolist()}
+    return jsonify(output)
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
---
+++ model.py
@@ -0,0 +1,97 @@
+import os
+import warnings
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet, Ridge, Lasso, LinearRegression
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from xgboost import XGBRegressor
+from urllib.parse import urlparse
+import mlflow
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn
+
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+def eval_metrics(actual, pred, num_features):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    mse = mean_squared_error(actual, pred)
+
+    # Calculate adjusted R-squared
+    n = len(actual)
+    k = num_features
+    r2_adj = 1 - ((1 - r2) * (n - 1) / (n - k - 1))
+
+    return rmse, mae, r2, r2_adj, mse
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    data = pd.read_csv("preprocessed_data.csv")
+
+    train, test = train_test_split(data)
+
+    train_x = train.drop(["charges"], axis=1)
+    test_x = test.drop(["charges"], axis=1)
+    train_y = train[["charges"]]
+    test_y = test[["charges"]]
+
+    regressors = [
+        {"name": "ElasticNet", "model": ElasticNet(), "params": {"alpha": 0.5, "l1_ratio": 0.5}},
+        {"name": "LinearRegression", "model": LinearRegression(), "params": {}},
+        {"name": "Ridge", "model": Ridge(), "params": {"alpha": 1.0}},
+        {"name": "Lasso", "model": Lasso(), "params": {"alpha": 1.0}},
+        {"name": "RandomForest", "model": RandomForestRegressor(), "params": {"n_estimators": 100, "random_state": 42}},
+        {"name": "DecisionTree", "model": DecisionTreeRegressor(), "params": {"random_state": 42}},
+        {"name": "SVR", "model": SVR(), "params": {"kernel": "linear"}},
+        {"name": "KNeighbors", "model": KNeighborsRegressor(), "params": {"n_neighbors": 5}},
+        {"name": "AdaBoost", "model": AdaBoostRegressor(), "params": {"n_estimators": 50, "random_state": 42}},
+        {"name": "XGBoost", "model": XGBRegressor(), "params": {"n_estimators": 100, "learning_rate": 0.1, "random_state": 42}},
+        {"name": "GradientBoosting", "model": GradientBoostingRegressor(), "params": {"n_estimators": 100, "random_state": 42}},
+    ]
+
+    for regressor in regressors:
+        with mlflow.start_run(run_name=regressor["name"]):
+            model_name = regressor["name"]
+            model = regressor["model"]
+            params = regressor["params"]
+
+            model.set_params(**params)
+
+            model.fit(train_x, train_y)
+            predicted_qualities = model.predict(test_x)
+
+            (rmse, mae, r2, r2_adj, mse) = eval_metrics(test_y, predicted_qualities, train_x.shape[1])
+
+            print(f"{model_name} model:")
+            print("  RMSE:", rmse)
+            print("  MAE:", mae)
+            print("  R2:", r2)
+            print("  Adjusted R2:", r2_adj)
+            print("  MSE:", mse)
+
+            mlflow.log_params(params)
+            mlflow.log_metric("rmse", rmse)
+            mlflow.log_metric("r2", r2)
+            mlflow.log_metric("mae", mae)
+            mlflow.log_metric("r2_adj", r2_adj)
+            mlflow.log_metric("mse", mse)
+
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            if tracking_url_type_store != "file":
+                mlflow.sklearn.log_model(model, "model", registered_model_name=f"{model_name}_Prediction")
+            else:
+                mlflow.sklearn.log_model(model, "model")
